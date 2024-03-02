16315ae4ed58ffd43c194c4de429a228b50252ca
sazhinsema <368749@edu.itmo.ru>
2024-03-02 06:29:18 +0300

    remove comments

--- banks-console/src/main/java/org/example/banks/Main.java
+++ banks-console/src/main/java/org/example/banks/Main.java
@@ -22,8 +22,6 @@ import org.example.banks.service.impl.UserServiceImpl;
 
 import java.util.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
